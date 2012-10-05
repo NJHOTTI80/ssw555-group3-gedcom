abdac8c36ab643f8e0830a27c8eee64c34098b6f
anthony.favia <affavia@gmail.com>
2012-10-04 21:21:26 -0400

    added both a unit test and one line in input test file for the story (all error and anomalies will be ouput and will tell where the error/anomaly occured)

--- src/GedcomTest.java
+++ src/GedcomTest.java
@@ -6,8 +6,30 @@ import org.junit.Test;
 public class GedcomTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAllErrorsAndAnomaliesAreOutput() {
+		Anomaly a = new Anomaly(1, "married to sibling");
+		Error e = new Error(3, "invalid tag");
+		
+		ProblemList pl = new ProblemList();
+		
+		pl.add(a);
+		pl.add(e);
+			
+		assertEquals(2, pl.size());
+		assertEquals(1, pl.get(0).getLineNumber());
+		assertEquals(3, pl.get(1).getLineNumber());
+		
+		assertEquals("married to sibling", pl.get(0).getMessage());
+		assertEquals("invalid tag", pl.get(1).getMessage());
+		
+		String s = pl.buildOutputString();
+		
+		assertTrue(s.contains("3"));
+		assertTrue(s.contains("1"));
+		assertTrue(s.contains("married to sibling"));
+		assertTrue(s.contains("invalid tag"));
+		
+		
 	}
 
 }
