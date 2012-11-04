d1f6380aaa18c270020efaf8fc6af8bf8a8a2790
Sean Maxon <cheshire.maxon@gmail.com>
2012-11-04 07:36:52 -0500

    Added necessary parts to ProblemFinder class for evaluating the entire file.  Completed multiple death dates function.

--- src/ProblemFinder.java
+++ src/ProblemFinder.java
@@ -1,12 +1,29 @@
 import java.util.GregorianCalendar;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Vector;
 
 //Evaluates whether there is an issue with a given family or individual
 
 public class ProblemFinder {
 	private ProblemList pl;
+	private Hashtable<String, Family> familyIndex;
+	private Hashtable<String, Individual> personIndex;
+	private Vector<String> listOfPeople;
+	private Vector<String> listOfFams;
 	
 	public ProblemFinder(){
 		pl = new ProblemList();
+	}
+	
+	public ProblemFinder(Hashtable<String, Family> fam, Hashtable<String, Individual> ind, Vector<String> people, Vector<String> families)
+	{
+		pl = new ProblemList();
+		familyIndex = fam;
+		personIndex = ind;
+		listOfPeople = people;
+		listOfFams = families;
 	}
 	
 	public static boolean isBirthDateAfterDeathDate(Individual i){
@@ -19,6 +36,10 @@ public class ProblemFinder {
 	}
 	
 	public boolean multiDeathCheck(Individual i){
+		ArrayList<GregorianCalendar> deaths;
+		deaths = i.getDeathDates();
+		if(deaths.size() > 1)
+			return true;
 		return false;
 	}
 	
@@ -29,5 +50,33 @@ public class ProblemFinder {
 	public boolean parentMarriage(Family f, Individual i1, Individual i2)
 	{
 		return false;
+	}
+	
+	//This function checks all families,  individuals for the errors and anomalies our program supports.
+	//Any boolean function above that returns true will add an entry to the problem list in this function.
+	//At the end of all error checking, this function will also print the output.
+	public void checkAllErrors()
+	{
+		String output;
+		Family f;
+		Individual i;
+		int k;
+		//Check each individual in the file for individual-level errors
+		for(k = 0; k < listOfPeople.size(); k++)
+		{
+			i = personIndex.get(listOfPeople.elementAt(k));
+			//Do all error checks on i
+			
+		}
+		
+		//Check each family in the file for family-level errors
+		for(k = 0; k < listOfFams.size(); k++)
+		{
+			f = familyIndex.get(listOfFams.elementAt(k));
+			//Do all error checks on f
+			
+		}
+		
+		output = pl.buildOutputString();
 	}
 }
